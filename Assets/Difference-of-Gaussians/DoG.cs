fb7ca0717bd4ae3ff72e779198820ec97b7586ff
nnakata <nakata@team-lab.com>
2014-02-04 19:09:44 +0900

    backup

--- Assets/Difference-of-Gaussians/DoG.cs
+++ Assets/Difference-of-Gaussians/DoG.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 [RequireComponent(typeof(Camera))]
 public class DoG : MonoBehaviour {
+	public const string PROP_SIGMA = "_Sigma";
+	public const string PROP_K = "_K";
+	public const string PROP_P = "_P";
+	public const string PROP_PHI = "_Phi";
+	public const string PROP_EPS = "_Eps";
+
 	public Material rgb2lab;
 	public Material lab2rgb;
 
@@ -27,5 +33,38 @@ public class DoG : MonoBehaviour {
 
 		RenderTexture.ReleaseTemporary(tmp0);
 		RenderTexture.ReleaseTemporary(tmp1);
+	}
+
+	void OnGUI() {
+		var prevSigma = dog.GetFloat(PROP_SIGMA);
+		var prevK = dog.GetFloat(PROP_K);
+		var prevP = dog.GetFloat(PROP_P);
+		var prevPhi = dog.GetFloat(PROP_PHI);
+		var prevEps = dog.GetFloat(PROP_EPS);
+
+		GUI.color = Color.green;
+		GUILayout.BeginVertical(GUILayout.Width(200));
+		GUILayout.Label("Sigma");
+		var tmpSigma = GUILayout.HorizontalSlider(prevSigma, 0.1f, 10f);
+		GUILayout.Label("K");
+		var tmpK = GUILayout.HorizontalSlider(prevK, 1.0f, 3.0f);
+		GUILayout.Label("P");
+		var tmpP = GUILayout.HorizontalSlider(prevP, 0f, 100f);
+		GUILayout.Label("Phi");
+		var tmpPhi = GUILayout.HorizontalSlider(prevPhi, 0f, 20f);
+		GUILayout.Label("Eps");
+		var tmpEps = GUILayout.HorizontalSlider(prevEps, 0f, 1f);
+		GUILayout.EndVertical();
+
+		if (tmpSigma != prevSigma)
+			dog.SetFloat(PROP_SIGMA, tmpSigma);
+		if (tmpK != prevK)
+			dog.SetFloat(PROP_K, tmpK);
+		if (tmpP != prevP)
+			dog.SetFloat(PROP_P, tmpP);
+		if (tmpPhi != prevPhi)
+			dog.SetFloat(PROP_PHI, tmpPhi);
+		if (tmpEps != prevEps)
+			dog.SetFloat(PROP_EPS, tmpEps);
 	}
 }
--- Assets/Gradient/Gradient.cs
+++ Assets/Gradient/Gradient.cs
@@ -4,26 +4,10 @@ using System.Collections;
 public class Gradient : MonoBehaviour {
 	public const string PROP_MAX_MAGNITUDE = "_Normalize";
 
-	public Transform screen;
-	public Material webcamMat;
-
 	public Material gradientMat;
 	public Material gradientMagMat;
 	public Material tangentMat;
 
-	private WebCamTexture _webcamTex;
-	private float _prevWebcamAspect = -1f;
-
-	// Use this for initialization
-	IEnumerator Start () {
-		yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
-
-		_webcamTex = new WebCamTexture();
-		_webcamTex.Play();
-		webcamMat.mainTexture = _webcamTex;
-
-		StartCoroutine("UpdateAspect");
-	}
 
 	void OnRenderImage(RenderTexture src, RenderTexture dst) {
 		var ping = RenderTexture.GetTemporary(src.width, src.height, 0, RenderTextureFormat.RGFloat);
@@ -49,20 +33,6 @@ public class Gradient : MonoBehaviour {
 		if (prevMaxMag != tmpMaxMag) {
 			prevMaxMag = tmpMaxMag;
 			gradientMagMat.SetFloat(PROP_MAX_MAGNITUDE, prevMaxMag);
-		}
-	}
-
-	IEnumerator UpdateAspect() {
-		while (true) {
-			yield return new WaitForSeconds(1f);
-			var webcamAspect = (float)_webcamTex.width / _webcamTex.height;
-			if (webcamAspect == _prevWebcamAspect)
-				continue;
-
-			_prevWebcamAspect = webcamAspect;
-			var s = screen.localScale;
-			s.x = s.y * webcamAspect;
-			screen.localScale = s;
 		}
 	}
 }
